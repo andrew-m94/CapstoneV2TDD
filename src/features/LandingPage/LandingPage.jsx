86f74d7e1d20748fb4c9c230f49b59394854e716
andrew morales <andrew.morales725@gmail.com>
2022-01-28 15:37:46 -0500

    Updated styling

--- src/features/LandingPage/LandingPage.jsx
+++ src/features/LandingPage/LandingPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './LandingPage.css'
 
 const LandingPage = () => {
     return (
-        <div>
+        <div className="container-fluid landing-page">
             <button className="btn btn-primary">sign in</button>
             <button className="btn btn-primary">sign up</button>
         </div>
